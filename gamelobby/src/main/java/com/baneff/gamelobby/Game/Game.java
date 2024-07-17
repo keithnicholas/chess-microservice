06a7d0db12be3eefba4f61ace2dee4fbe5db7e6e
igat <iggyspamacc@gmail.com>
2024-07-18 01:16:49 +0800

    polish

--- gamelobby/src/main/java/com/baneff/gamelobby/Game/Game.java
+++ gamelobby/src/main/java/com/baneff/gamelobby/Game/Game.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 
 import java.time.LocalDateTime;
 
